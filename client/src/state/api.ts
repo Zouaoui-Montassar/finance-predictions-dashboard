ecc1b069427eb7785825b41c5bef0f3289cd464d
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-11-21 17:54:03 +0100

    Row 1 on the dashboard done + its cards

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./types";
 /* Key performance indicators */
 
 
@@ -7,7 +8,7 @@ export const api = createApi({
     reducerPath :"main",
     tagTypes: ["Kpis"],
     endpoints: (build) => ({
-        getKpis:build.query<void, void>({
+        getKpis:build.query<Array<GetKpisResponse>, void>({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"]
         })
